23fdc473f67172c6da364c04d94f2ed58b664906
Ravi <ravi.parsa@mobileprogrammingllc.com>
2020-05-22 02:04:58 +0530

    code clean up

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -1,10 +1,8 @@
 //
 //  AppDelegate.swift
 //  HelloWorld
-//
 //  Created by RAVI PARSA on 14/05/20.
 //  Copyright © 2020 RAVI PARSA. All rights reserved.
-//
 
 import Cocoa
 
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,10 +1,8 @@
 //
 //  ViewController.swift
 //  HelloWorld
-//
 //  Created by RAVI PARSA on 14/05/20.
 //  Copyright © 2020 RAVI PARSA. All rights reserved.
-//
 
 import Cocoa
 
@@ -17,7 +15,6 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
